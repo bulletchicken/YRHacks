07a55a612a962f1ab6f930f68ab8454be97d2a7f
Darren Liu <109255883+darren4141@users.noreply.github.com>
2023-03-03 22:46:47 -0500

    basic homepage

---
+++ script.js
@@ -0,0 +1,11 @@
+function homepage(){
+    window.location ="homepage.html";
+}
+
+function chat(){
+    window.location = "chat.html";
+}
+
+function track(){
+    window.location = "track.html";
+}
