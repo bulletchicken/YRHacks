eec4559195eee5bd4da7d937df3a51188650603f
darren4141 <109255883+darren4141@users.noreply.github.com>
2023-03-04 00:16:15 -0500

    more homepage

--- public/script.js
+++ public/script.js
@@ -6,6 +6,14 @@ function chat(){
     window.location = "chat.html";
 }
 
-function track(){
+function trackElbow(){
+    window.location = "trackerElbow.html";
+}
+
+function trackLeg(){
+    window.location = "trackerLeg.html";
+}
+
+function trackShoulder(){
     window.location = "trackerShoulder.html";
 }
