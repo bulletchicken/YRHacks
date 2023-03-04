610de6e8828a3131edf2e0ee82a8190b8ba19770
Jeremy Su <jeremysu64@gmail.com>
2023-03-03 22:29:00 -0500

    added tracker and template of website

---
+++ public/detect.js
@@ -0,0 +1,65 @@
+//import * as ITMES from "script.js";
+
+let my_cam;
+let posenet;
+let noseX,noseY;
+let reyeX,reyeY;
+let leyeX,leyeY;
+let pose,skeleton;
+let actor_img;
+let specs,smoke;
+
+
+let target = document.getElementById('') 
+
+function setup() {
+
+    var canvas = createCanvas(900, 500);
+    my_cam = createCapture(VIDEO)
+    
+    my_cam.size(900, 500)
+    my_cam.hide();
+    posenet = ml5.poseNet(my_cam, modelLoaded);
+    posenet.on('pose',receivedPoses);
+}
+
+
+function receivedPoses(poses){
+
+    //work here to run functions
+    
+
+    if(poses.length > 0){
+        pose = poses[0].pose;
+        skeleton = poses[0].skeleton;
+    }
+
+    //calculate range of motion
+
+    
+
+    //const scale = Math.abs(pose.leftShoulder.x-pose.leftElbow.y)
+    console.log(poses[0]);
+}
+
+function modelLoaded() {
+    console.log('Model has loaded');
+}
+
+function draw() {
+    
+    image(my_cam, 0, 0, 900, 500);
+    fill(255,255,255);
+
+    if(pose){
+        for(let i=0; i<pose.keypoints.length; i++){
+            ellipse(pose.keypoints[i].position.x, pose.keypoints[i].position.y,20);
+        }
+        stroke(255,255,255);
+        strokeWeight(3);
+        for(let j=0; j<skeleton.length; j++){
+            line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y)
+        }
+    }
+
+}
